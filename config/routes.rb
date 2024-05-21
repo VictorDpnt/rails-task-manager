75e3a874e3ff71ad613f4aaeac1da44cfc46aeba
victor dupont <victordupont1407@gmail.com>
2024-05-21 16:22:30 +0200

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  get '/tasks', to: 'tasks#index', as: :tasks
+  post '/tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/new', to: 'tasks#new', as: :new
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
   # root "posts#index"
 end
